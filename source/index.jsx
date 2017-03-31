03cb43d35bb09f8e49b09d97d5eb93902f165e2a
Raouf Aghrout <raouf.aghrout@unrulygroup.com>
2017-03-31 11:59:02 +0100

    Style change.

--- source/index.jsx
+++ source/index.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <h1 className="page-heading">Hello, world!</h1>,
-  document.getElementById('ROOT')
+    <h1 className="page-heading">Hello, world!</h1>,
+    document.getElementById('ROOT')
 );
